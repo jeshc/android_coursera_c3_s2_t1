cf3d27a3ac158efd45e445c3f7088556fc465658
jeshc <hcjesus@unam.mx>
2021-10-11 12:02:47 -0500

    avance

---
+++ app/src/main/java/com/example/myapplication/Contacto.java
@@ -0,0 +1,33 @@
+package com.example.myapplication;
+
+import java.util.Date;
+
+public class Contacto {
+    private String nombre;
+    private Date fechaNacimiento;
+    private String telefono;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Date getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(Date fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+}
